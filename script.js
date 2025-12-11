240059eb13baab72ddbbc75adb7ad2e9de9cec30
Daniel Ingber <diiamfbasfd@gmail.com>
2025-12-11 12:34:11 +0200

    lots of codddddd

---
+++ script.js
@@ -0,0 +1,30 @@
+// קביעת משתנים קבועים לשימוש בפונקציות (משתנים שלא יישתנו ולכן הם בconst)
+const toggleButton = document.getElementById('toggleButton')
+const sidebar = document.getElementById('sidebar')
+// פונקציה לפתיחת תפריט הניווט על ידי שינוי המחלקות שלהם (בשימוש עם toggle)
+function toggleSidebar(){
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
+    closeAllSubMenus()
+}
+
+function toggleSubMenu(button){
+// אם כפתור לא במחלקה של visible אז תריץ את הפונקציה שסוגרת את כל תתי הניווטים 
+    if(!button.nextElementSibling.classList.contains('visible')){closeAllSubMenus()}
+    button.nextElementSibling.classList.toggle('visible')
+    button.classList.toggle('rotate')
+// אם קיים בsidebar המחלקה close אז תוריד גם את close וגם לtoggleButton תשנה את rotate 
+    if(sidebar.classList.contains('close')){
+        sidebar.classList.toggle('close')
+        toggleButton.classList.toggle('rotate')
+}
+}
+
+function closeAllSubMenus(){
+  // לולאה שבודקת את כל הילדים של sidebar בשביל כל האלמנטים עם המחלקה visible ומריצה לולאה שבשביל כל ul בarray מורידים לו את המחלקה visible וגם מוחקת את המחלקה rotate בשביל אנימציה של שינוי הicon 
+    Array.from(sidebar.getElementsByClassName('visible')).forEach(ul => {
+        ul.classList.remove('visible')
+        ul.previousElementSibling.classList.remove('rotate')})
+}
+
+// מכיוון שבזמן זה עוד לא למדנו JS קוד זה הוא לא קוד מקורי שלי ועקבתי אחרי השיעור הנ"ל :https://www.youtube.com/watch?v=R7b3OlEyqug 
