6121a2a04ba9e35e63e49c8ce97e5197e30686a5
Daniel Ingber <diiamfbasfd@gmail.com>
2025-12-15 17:47:53 +0200

    added

--- script.js
+++ script.js
@@ -10,21 +10,23 @@ function toggleSidebar(){
 
 function toggleSubMenu(button){
 // אם כפתור לא במחלקה של visible אז תריץ את הפונקציה שסוגרת את כל תתי הניווטים 
-    if(!button.nextElementSibling.classList.contains('visible')){closeAllSubMenus()}
-    button.nextElementSibling.classList.toggle('visible')
+if(!button.nextElementSibling.classList.contains('show')){
+    closeAllSubMenus()
+}
+    button.nextElementSibling.classList.toggle('show')
     button.classList.toggle('rotate')
 // אם קיים בsidebar המחלקה close אז תוריד גם את close וגם לtoggleButton תשנה את rotate 
     if(sidebar.classList.contains('close')){
-        sidebar.classList.toggle('close')
-        toggleButton.classList.toggle('rotate')
-}
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
+    }
 }
 
 function closeAllSubMenus(){
   // לולאה שבודקת את כל הילדים של sidebar בשביל כל האלמנטים עם המחלקה visible ומריצה לולאה שבשביל כל ul בarray מורידים לו את המחלקה visible וגם מוחקת את המחלקה rotate בשביל אנימציה של שינוי הicon 
-    Array.from(sidebar.getElementsByClassName('visible')).forEach(ul => {
-        ul.classList.remove('visible')
-        ul.previousElementSibling.classList.remove('rotate')})
+    Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+    ul.classList.remove('show')
+    ul.previousElementSibling.classList.remove('rotate')})
 }
 
 // מכיוון שבזמן זה עוד לא למדנו JS קוד זה הוא לא קוד מקורי שלי ועקבתי אחרי השיעור הנ"ל :https://www.youtube.com/watch?v=R7b3OlEyqug 
